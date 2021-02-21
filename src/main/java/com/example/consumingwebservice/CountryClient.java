3064a4c1bdb574379e2400de7316fe19c4498abe
Eric Karanja <ekaranja@cellulant.com>
2021-02-21 14:30:31 +0300

    initial commit

---
+++ src/main/java/com/example/consumingwebservice/CountryClient.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.consumingwebservice;
+
+import com.example.consumingwebservice.wsdl.GetCountryRequest;
+import com.example.consumingwebservice.wsdl.GetCountryResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
+import org.springframework.ws.soap.client.core.SoapActionCallback;
+
+/**
+ *
+ * @author ekaranja
+ */
+public class CountryClient extends WebServiceGatewaySupport {
+
+    private static final Logger log = LoggerFactory.getLogger(CountryClient.class);
+
+    public GetCountryResponse getCountry(String country) {
+
+        GetCountryRequest request = new GetCountryRequest();
+        request.setName(country);
+
+        log.info("Requesting location for " + country);
+
+        GetCountryResponse response = (GetCountryResponse) getWebServiceTemplate()
+                .marshalSendAndReceive("http://localhost:8080/ws/countries", request,
+                        new SoapActionCallback(
+                                "http://spring.io/guides/gs-producing-web-service/GetCountryRequest"));
+
+        return response;
+    }
+
+}
---
+++ src/main/java/com/example/consumingwebservice/CountryConfiguration.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.consumingwebservice;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+
+/**
+ *
+ * @author ekaranja
+ */
+@Configuration
+public class CountryConfiguration {
+
+    @Bean
+    public Jaxb2Marshaller marshaller() {
+        Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
+        // this package must match the package in the <generatePackage> specified in
+        // pom.xml
+        marshaller.setContextPath("com.example.consumingwebservice.wsdl");
+        return marshaller;
+    }
+
+    @Bean
+    public CountryClient countryClient(Jaxb2Marshaller marshaller) {
+        CountryClient client = new CountryClient();
+        client.setDefaultUri("http://localhost:8080/ws");
+        client.setMarshaller(marshaller);
+        client.setUnmarshaller(marshaller);
+        return client;
+    }
+}
